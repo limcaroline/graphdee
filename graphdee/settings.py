e904b5fffae91483907d86ac669e66ac1a5dc1c5
limcaroline <carolineloislim@gmail.com>
2025-10-10 20:54:05 +0800

    Added env.py and gitignore for it

--- graphdee/settings.py
+++ graphdee/settings.py
@@ -18,6 +18,10 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR: Path = Path(__file__).resolve().parent.parent
 
+# Load local env vars if env.py exists (ignored in Git)
+if (BASE_DIR / "env.py").exists():
+    import env  # noqa: F401
+
 # Quick-start development settings - unsuitable for production
 # See:
 # https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
