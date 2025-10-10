c5154ccaa11adda5f16c89d667f261eb9fa7d666
limcaroline <carolineloislim@gmail.com>
2025-10-10 23:29:56 +0800

    use SQLite locally when DEVELOPMENT flag is set

--- graphdee/settings.py
+++ graphdee/settings.py
@@ -108,24 +108,21 @@ WSGI_APPLICATION: str = 'graphdee.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-db_url = os.environ.get("DATABASE_URL")
-
-if db_url:
-    # Use PostgreSQL (on Heroku / CI)
+# Use SQLite in development; Postgres in production (Heroku)
+if 'DEVELOPMENT' in os.environ:
     DATABASES = {
-        "default": dj_database_url.parse(
-            db_url,
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(
+            os.environ.get('DATABASE_URL'),
             conn_max_age=600,
             ssl_require=True,
         )
-    }
-else:
-    # Use SQLite locally
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
     }
 
 # Authentication / django-allauth
