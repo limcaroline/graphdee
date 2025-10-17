29715f3c70c72647826f09ab8cf260c6d5e41580
limcaroline <carolineloislim@gmail.com>
2025-10-17 19:51:32 +0800

    Updated payment success again while debugging

--- orders/urls.py
+++ orders/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.create_order, name="create_order"),
+    path("success/", views.payment_success, name="payment_success"),
     path("my/", views.my_orders, name="my_orders"),
 ]
