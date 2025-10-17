041eabf8322bb0664a78a2efb1889213ec1bfead
limcaroline <carolineloislim@gmail.com>
2025-10-17 19:58:42 +0800

    Added app name for orders

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "orders"
+
 urlpatterns = [
     path("", views.create_order, name="create_order"),
     path("success/", views.payment_success, name="payment_success"),
