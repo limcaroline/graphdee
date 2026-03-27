bd50f9209d6c88a407d85832ed5f3519620957b0
limcaroline <carolineloislim@gmail.com>
2026-03-27 01:09:25 +0100

    Fix prod database

--- graphdee/settings.py
+++ graphdee/settings.py
@@ -116,10 +116,11 @@ WSGI_APPLICATION: str = 'graphdee.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 # Use SQLite in development; Postgres in production (Heroku)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 # Authentication / django-allauth
