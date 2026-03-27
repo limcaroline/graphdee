9cf9bf4c80e3ad6b0f32065c089904a0f2ee0578
limcaroline <carolineloislim@gmail.com>
2026-03-27 01:32:49 +0100

    Updated cancel functionality

--- graphdee/settings.py
+++ graphdee/settings.py
@@ -115,13 +115,23 @@ WSGI_APPLICATION: str = 'graphdee.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 # Use SQLite in development; Postgres in production (Heroku)
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True,
-    )
-}
+if os.getenv("DATABASE_URL"):
+    # Production (Heroku)
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.getenv("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True,
+        )
+    }
+else:
+    # Local development
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Authentication / django-allauth
 AUTHENTICATION_BACKENDS: tuple[str, ...] = (
